76348e52a27069045ef9e2936b0097a3a0a43d99
Nikita Vilunov <nikitaoryol@gmail.com>
2017-12-10 13:32:36 +0300

    Config in a file now

--- src/main/scala/Vars.scala
+++ src/main/scala/Vars.scala
@@ -1,16 +1,20 @@
+import com.typesafe.config._
+
 object Vars {
+  var config: Config = ConfigFactory.load()
+
   object TgClient {
-    val ApiId = 0
-    val ApiHash = ""
-    val SessionName = "tglogger"
-    val SessionFilePath = "session"
+    val ApiId: Int              = config.getInt   ("telegram.api_id")
+    val ApiHash: String         = config.getString("telegram.api_hash")
+    val SessionName: String     = config.getString("telegram.session_name")
+    val SessionFilePath: String = config.getString("telegram.session_file_path")
   }
 
   object DB {
-    val DBName = "tglogger"
-    val User = "tglogger"
-    val Password = ""
-    val Host = "localhost"
+    val DBName: String   = config.getString("database.db")
+    val User: String     = config.getString("database.user")
+    val Password: String = config.getString("database.password")
+    val Host: String     = config.getString("database.hostname")
     val ConnStr = s"jdbc:postgresql://$Host/$DBName?user=$User"
   }
 }
